c606d242789d44c625e80db14b9d772f1c2a86fe
BaeHyenMin <168337879+BaeHyenMin@users.noreply.github.com>
2024-05-12 20:56:29 +0900

    20240512
    페이스북 파일 컨트롤러 추가

--- facebook/src/main/java/com/exam/facebook/controller/BoardView.java
+++ facebook/src/main/java/com/exam/facebook/controller/BoardView.java
@@ -1,18 +1,34 @@
 package com.exam.facebook.controller;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.exam.facebook.dto.FileResponse;
+import com.exam.facebook.service.FileService;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 public class BoardView {
+
+	private final FileService fileService;
 	
-	
-	@GetMapping("/img/{name}")
-	public Byte[] imgView(@PathVariable("name") String img) {
-		return null;
+	@GetMapping("/uploads/{name}")
+	@ResponseBody
+	public ResponseEntity<byte[]> getFile(@PathVariable("name") String fileName) {
+		FileResponse res = fileService.getFile(fileName);
+		if (res.getContentType() != null) {
+			HttpHeaders header = new HttpHeaders();
+			header.add("Content-Type", res.getContentType());
+			return new ResponseEntity<>(res.getBytes(), header, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
 	}
 }
